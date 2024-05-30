e811649a527009f75d310c3648db57fa95e62f6d
erwin-huang <erwinhuank@gmail.com>
2024-05-30 16:05:10 +0700

    feat: add login and register feature

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,18 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::controller(AuthController::class)->group(function () {
+    Route::post('/auth/login', 'login');
+    Route::post('/auth/register', 'register');
+    // Route::post('/logout', 'logout')->middleware('auth:sanctum');
+});
+
+Route::middleware('auth:sanctum')->group( function () {
+    Route::controller(UserController::class)->group(function () {
+        Route::get('/users/me', 'me');
+    });
+});
