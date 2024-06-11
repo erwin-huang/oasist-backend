bf705cfb4c5f1264d5974d6ea61ff797b96e0b28
erwin-huang <erwinhuank@gmail.com>
2024-06-11 21:15:43 +0700

    feat: create `/templates` api and `/template/{id}` api

---
+++ app/Http/Controllers/TemplateController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\TemplateResource;
+use App\Models\Template;
+use Illuminate\Http\Response;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Throwable;
+use Illuminate\Http\Request;
+
+class TemplateController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request)
+    {
+        $perPage = $request->per_page ?? 10;
+        return TemplateResource::collection(Template::paginate($perPage));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        try {
+            $template = Template::with(['templateSections', 'templateSections.templateValues'])->findOrFail($id);
+            return TemplateResource::make($template);
+        } catch (ModelNotFoundException $e) {
+            return response()->json(['message' => 'Template not found'], 404);
+        } catch (Throwable $e) {
+            return response()->json(['message' => $e->getMessage()], 500);
+        }
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,8 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TemplateController;
 use App\Http\Controllers\UserController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::controller(AuthController::class)->group(function () {
@@ -11,8 +11,10 @@ Route::controller(AuthController::class)->group(function () {
     Route::post('/auth/logout', 'logout')->middleware('auth:sanctum');
 });
 
-Route::middleware('auth:sanctum')->group( function () {
+Route::middleware('auth:sanctum')->group(function () {
     Route::controller(UserController::class)->group(function () {
         Route::get('/users/me', 'me');
     });
 });
+
+Route::resource('templates', TemplateController::class)->only(['index', 'show']);
